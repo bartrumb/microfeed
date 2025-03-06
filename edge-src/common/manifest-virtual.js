b424908e8c11144a1e43ae8d4ead6770fadc915c
Brett Bartrum <brett@juicedwheels.com>
2025-03-05 19:49:31 -0600

    Add TypeScript configuration for Node and update deployment scripts to use ts-node

---
+++ edge-src/common/manifest-virtual.js
@@ -0,0 +1,420 @@
+// This file is auto-generated during build
+// Do not edit manually
+
+export const manifestData = {
+  "_BrowserUtils.js": {
+    "file": "_app/immutable/chunks/BrowserUtils.js",
+    "name": "BrowserUtils",
+    "imports": [
+      "_react-vendor.js",
+      "_ToastUtils.js",
+      "_StringUtils.js",
+      "_index3.js"
+    ]
+  },
+  "_Constants.js": {
+    "file": "_app/immutable/chunks/Constants.js",
+    "name": "Constants"
+  },
+  "_StringUtils.js": {
+    "file": "_app/immutable/chunks/StringUtils.js",
+    "name": "StringUtils",
+    "imports": [
+      "_utils.js"
+    ]
+  },
+  "_ToastUtils.js": {
+    "file": "_app/immutable/chunks/ToastUtils.js",
+    "name": "ToastUtils",
+    "imports": [
+      "_react-vendor.js"
+    ]
+  },
+  "_admin_styles.js": {
+    "file": "_app/immutable/chunks/admin_styles.js",
+    "name": "admin_styles",
+    "imports": [
+      "_react-vendor.js",
+      "_StringUtils.js",
+      "_Constants.js"
+    ]
+  },
+  "_index.js": {
+    "file": "_app/immutable/chunks/index.js",
+    "name": "index",
+    "imports": [
+      "_react-vendor.js"
+    ]
+  },
+  "_index2.js": {
+    "file": "_app/immutable/chunks/index2.js",
+    "name": "index",
+    "imports": [
+      "_react-vendor.js"
+    ]
+  },
+  "_index3.js": {
+    "file": "_app/immutable/chunks/index3.js",
+    "name": "index",
+    "imports": [
+      "_react-vendor.js"
+    ]
+  },
+  "_index4.js": {
+    "file": "_app/immutable/chunks/index4.js",
+    "name": "index",
+    "imports": [
+      "_react-vendor.js",
+      "_index5.js",
+      "_StringUtils.js"
+    ]
+  },
+  "_index5.js": {
+    "file": "_app/immutable/chunks/index5.js",
+    "name": "index",
+    "imports": [
+      "_react-vendor.js"
+    ]
+  },
+  "_react-vendor.js": {
+    "file": "_app/immutable/chunks/react-vendor.js",
+    "name": "react-vendor"
+  },
+  "_ui-components.js": {
+    "file": "_app/immutable/chunks/ui-components.js",
+    "name": "ui-components",
+    "imports": [
+      "_react-vendor.js"
+    ]
+  },
+  "_utils.js": {
+    "file": "_app/immutable/chunks/utils.js",
+    "name": "utils",
+    "imports": [
+      "_react-vendor.js"
+    ]
+  },
+  "_withManifest.js": {
+    "file": "_app/immutable/chunks/withManifest.js",
+    "name": "withManifest",
+    "imports": [
+      "_react-vendor.js",
+      "_index5.js"
+    ]
+  },
+  "client-src/ClientAdminChannelApp/index.tsx": {
+    "file": "_app/immutable/entry-adminchannel.js",
+    "name": "adminchannel",
+    "src": "client-src/ClientAdminChannelApp/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_ToastUtils.js",
+      "_admin_styles.js",
+      "_BrowserUtils.js",
+      "_index2.js",
+      "_StringUtils.js",
+      "_index3.js",
+      "_Constants.js",
+      "_index.js"
+    ]
+  },
+  "client-src/ClientAdminCustomCodeEditorApp/index.tsx": {
+    "file": "_app/immutable/entry-admincustomcode.js",
+    "name": "admincustomcode",
+    "src": "client-src/ClientAdminCustomCodeEditorApp/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_admin_styles.js",
+      "_ui-components.js",
+      "_Constants.js",
+      "_index.js"
+    ]
+  },
+  "client-src/ClientAdminHomeApp/index.tsx": {
+    "file": "_app/immutable/entry-adminhome.js",
+    "name": "adminhome",
+    "src": "client-src/ClientAdminHomeApp/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_Constants.js",
+      "_admin_styles.js",
+      "_StringUtils.js",
+      "_index3.js",
+      "_ToastUtils.js"
+    ]
+  },
+  "client-src/ClientAdminItemsApp/index.tsx": {
+    "file": "_app/immutable/entry-adminitems.js",
+    "name": "adminitems",
+    "src": "client-src/ClientAdminItemsApp/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_admin_styles.js",
+      "_StringUtils.js",
+      "_Constants.js",
+      "_index3.js",
+      "_index2.js"
+    ]
+  },
+  "client-src/ClientAdminSettingsApp/index.tsx": {
+    "file": "_app/immutable/entry-adminsettings.js",
+    "name": "adminsettings",
+    "src": "client-src/ClientAdminSettingsApp/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_admin_styles.js",
+      "_BrowserUtils.js",
+      "_index2.js",
+      "_Constants.js",
+      "_ToastUtils.js",
+      "_StringUtils.js",
+      "_index.js"
+    ]
+  },
+  "functions/admin/_middleware.ts": {
+    "file": "functions/admin/_middleware.js",
+    "name": "functions/admin/_middleware",
+    "src": "functions/admin/_middleware.ts",
+    "isEntry": true
+  },
+  "functions/admin/ajax/feed.js": {
+    "file": "functions/admin/ajax/feed.js",
+    "name": "functions/admin/ajax/feed",
+    "src": "functions/admin/ajax/feed.js",
+    "isEntry": true,
+    "imports": [
+      "_utils.js"
+    ]
+  },
+  "functions/admin/ajax/r2-ops.js": {
+    "file": "functions/admin/ajax/r2-ops.js",
+    "name": "functions/admin/ajax/r2-ops",
+    "src": "functions/admin/ajax/r2-ops.js",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx"
+    ]
+  },
+  "functions/admin/channels/index.tsx": {
+    "file": "functions/admin/channels/index.js",
+    "name": "functions/admin/channels/index",
+    "src": "functions/admin/channels/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_utils.js"
+    ]
+  },
+  "functions/admin/channels/primary/index.tsx": {
+    "file": "functions/admin/channels/primary/index.js",
+    "name": "functions/admin/channels/primary/index",
+    "src": "functions/admin/channels/primary/index.tsx",
+    "isEntry": true
+  },
+  "functions/admin/feed/json.tsx": {
+    "file": "functions/admin/feed/json.js",
+    "name": "functions/admin/feed/json",
+    "src": "functions/admin/feed/json.tsx",
+    "isEntry": true
+  },
+  "functions/admin/index.tsx": {
+    "file": "functions/admin/index.js",
+    "name": "functions/admin/index",
+    "src": "functions/admin/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx",
+      "_index4.js",
+      "_Constants.js",
+      "_withManifest.js"
+    ]
+  },
+  "functions/admin/items/[itemId]/index.jsx": {
+    "file": "functions/admin/items/_itemId_/index.js",
+    "name": "functions/admin/items/_itemId_/index",
+    "src": "functions/admin/items/[itemId]/index.jsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_index4.js",
+      "_Constants.js",
+      "_index5.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx"
+    ]
+  },
+  "functions/admin/items/index.tsx": {
+    "file": "functions/admin/items/index.js",
+    "name": "functions/admin/items/index",
+    "src": "functions/admin/items/index.tsx",
+    "isEntry": true
+  },
+  "functions/admin/items/list/index.tsx": {
+    "file": "functions/admin/items/list/index.js",
+    "name": "functions/admin/items/list/index",
+    "src": "functions/admin/items/list/index.tsx",
+    "isEntry": true
+  },
+  "functions/admin/items/new/index.tsx": {
+    "file": "functions/admin/items/new/index.js",
+    "name": "functions/admin/items/new/index",
+    "src": "functions/admin/items/new/index.tsx",
+    "isEntry": true
+  },
+  "functions/admin/settings/code-editor/index.tsx": {
+    "file": "functions/admin/settings/code-editor/index.js",
+    "name": "functions/admin/settings/code-editor/index",
+    "src": "functions/admin/settings/code-editor/index.tsx",
+    "isEntry": true
+  },
+  "functions/admin/settings/index.tsx": {
+    "file": "functions/admin/settings/index.js",
+    "name": "functions/admin/settings/index",
+    "src": "functions/admin/settings/index.tsx",
+    "isEntry": true
+  },
+  "functions/api/_middleware.ts": {
+    "file": "functions/api/_middleware.js",
+    "name": "functions/api/_middleware",
+    "src": "functions/api/_middleware.ts",
+    "isEntry": true
+  },
+  "functions/api/channels/[channelId]/index.jsx": {
+    "file": "functions/api/channels/_channelId_/index.js",
+    "name": "functions/api/channels/_channelId_/index",
+    "src": "functions/api/channels/[channelId]/index.jsx",
+    "isEntry": true
+  },
+  "functions/api/feed/index.ts": {
+    "file": "functions/api/feed/index.js",
+    "name": "functions/api/feed/index",
+    "src": "functions/api/feed/index.ts",
+    "isEntry": true
+  },
+  "functions/api/items/[itemId]/index.jsx": {
+    "file": "functions/api/items/_itemId_/index.js",
+    "name": "functions/api/items/_itemId_/index",
+    "src": "functions/api/items/[itemId]/index.jsx",
+    "isEntry": true,
+    "imports": [
+      "_utils.js",
+      "_react-vendor.js",
+      "functions/i/[slug]/rss/index.jsx",
+      "functions/admin/ajax/r2-ops.js"
+    ]
+  },
+  "functions/api/items/index.tsx": {
+    "file": "functions/api/items/index.js",
+    "name": "functions/api/items/index",
+    "src": "functions/api/items/index.tsx",
+    "isEntry": true
+  },
+  "functions/api/media_files/presigned_urls/index.tsx": {
+    "file": "functions/api/media_files/presigned_urls/index.js",
+    "name": "functions/api/media_files/presigned_urls/index",
+    "src": "functions/api/media_files/presigned_urls/index.tsx",
+    "isEntry": true
+  },
+  "functions/i/[slug]/index.jsx": {
+    "file": "functions/i/_slug_/index.js",
+    "name": "functions/i/_slug_/index",
+    "src": "functions/i/[slug]/index.jsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_index5.js",
+      "_StringUtils.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx"
+    ]
+  },
+  "functions/i/[slug]/json/index.jsx": {
+    "file": "functions/i/_slug_/json/index.js",
+    "name": "functions/i/_slug_/json/index",
+    "src": "functions/i/[slug]/json/index.jsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx",
+      "functions/admin/ajax/r2-ops.js"
+    ]
+  },
+  "functions/i/[slug]/rss/index.jsx": {
+    "file": "functions/i/_slug_/rss/index.js",
+    "name": "functions/i/_slug_/rss/index",
+    "src": "functions/i/[slug]/rss/index.jsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_utils.js"
+    ]
+  },
+  "functions/index.tsx": {
+    "file": "functions/index.js",
+    "name": "functions/index",
+    "src": "functions/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_index4.js",
+      "_Constants.js",
+      "_withManifest.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx"
+    ]
+  },
+  "functions/json/index.tsx": {
+    "file": "functions/json/index.js",
+    "name": "functions/json/index",
+    "src": "functions/json/index.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx",
+      "functions/admin/ajax/r2-ops.js"
+    ]
+  },
+  "functions/json/openapi.yaml/index.tsx": {
+    "file": "functions/json/openapi.yaml/index.js",
+    "name": "functions/json/openapi.yaml/index",
+    "src": "functions/json/openapi.yaml/index.tsx",
+    "isEntry": true
+  },
+  "functions/rss/index.tsx": {
+    "file": "functions/rss/index.js",
+    "name": "functions/rss/index",
+    "src": "functions/rss/index.tsx",
+    "isEntry": true
+  },
+  "functions/rss/stylesheet.tsx": {
+    "file": "functions/rss/stylesheet.js",
+    "name": "functions/rss/stylesheet",
+    "src": "functions/rss/stylesheet.tsx",
+    "isEntry": true
+  },
+  "functions/sitemap.xml.tsx": {
+    "file": "functions/sitemap.xml.js",
+    "name": "functions/sitemap.xml",
+    "src": "functions/sitemap.xml.tsx",
+    "isEntry": true,
+    "imports": [
+      "_react-vendor.js",
+      "_utils.js",
+      "functions/i/[slug]/rss/index.jsx"
+    ]
+  },
+  "style.css": {
+    "file": "_app/immutable/assets/index.css",
+    "src": "style.css"
+  }
+};
